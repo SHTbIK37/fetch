85b96385a61c0e21adbd8c1e74515208db44d761
Ivan Shtykov <44865669+SHTbIK37@users.noreply.github.com>
2022-08-15 14:24:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Создай отдельный репозиторий для этого
+// Запросить список 20 запусков с сервера (SpaceX API, https://docs.spacexdata.com/#5fc4c846-c373-43df-a10a-e9faf80a8b0a)
+// и построить таблицу с полученными данными
+// -- launch_year, mission_name, rocket -> rocket_name, flight_number
+// Добавить клиентскую сортировку по клику на заголовок mission_name
+let result;
+async function get() {
+  const url =
+    "https://docs.spacexdata.com/#5fc4c846-c373-43df-a10a-e9faf80a8b0a";
+  result = await fetch(url);
+  return result;
+}
+get();
+console.log(result);
