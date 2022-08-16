ce9f291227d2778d23c980d18bcf77db39498d4a
Ivan <ishtykov0@gmail.com>
2022-08-16 16:10:06 +0300

    tasks done

--- script.js
+++ script.js
@@ -3,24 +3,75 @@
 // и построить таблицу с полученными данными
 // -- launch_year, mission_name, rocket -> rocket_name, flight_number
 // Добавить клиентскую сортировку по клику на заголовок mission_name
-// добавить состояние загрузки - loading...
-// добавить состояние ошибки - вывести пользователю
+// добавить состояние загрузки - loading... +
+// добавить состояние ошибки - вывести пользователю +
+// сортировка готова проблема что я вынес launches в глобал
 
 (async function () {
   const spaceXUrl = "https://api.spacexdata.com/v3/launches?limit=20";
-
+  let launches;
+  showLoading();
   try {
-    const launches = await getLaunches();
-    for (let i = 0; i < launches.length; i++) {
-      addLaunch(launches[i]);
-    }
+    launches = await getLaunches();
+    hideLoading();
+    createTable();
   } catch (error) {
-    console.error("MY ERROR", error);
+    hideLoading();
+    showError(error);
   }
   let columnHeader = document.getElementById("sort");
   columnHeader.addEventListener("click", sort);
 
   // *****************************************
+
+  function createTable() {
+    for (let i = 0; i < launches.length; i++) {
+      addLaunch(launches[i]);
+    }
+  }
+
+  function deleteRows() {
+    let table = document.getElementById("table");
+    let firstRow = table.rows.length;
+    for (let i = table.rows.length - 1; i > 0; i--) {
+      let time = table.rows[i];
+      table.removeChild(time);
+    }
+  }
+
+  function bubbleSortConcept() {
+    for (let j = launches.length - 1; j > 0; j--) {
+      for (let i = 0; i < j; i++) {
+        if (launches[i].mission_name > launches[i + 1].mission_name) {
+          let temp = launches[i];
+          launches[i] = launches[i + 1];
+          launches[i + 1] = temp;
+        }
+      }
+    }
+  }
+
+  function sort() {
+    deleteRows();
+    bubbleSortConcept();
+    createTable();
+  }
+
+  function showError(error) {
+    alert("Error!\n" + error);
+  }
+
+  function showLoading() {
+    let loadingP = document.createElement("p");
+    loadingP.setAttribute("id", "load");
+    loadingP.innerHTML = "Loading...";
+    document.body.append(loadingP);
+  }
+
+  function hideLoading() {
+    let removeElem = document.getElementById("load");
+    document.body.removeChild(removeElem);
+  }
 
   async function getLaunches() {
     const result = await fetch(spaceXUrl);
@@ -30,7 +81,6 @@
   function addLaunch(launch) {
     let tableElem = document.getElementById("table");
     let newstring = document.createElement("tr");
-    newstring.setAttribute("id", "tr");
     tableElem.append(newstring);
     let year = document.createElement("td");
     year.innerHTML = launch.launch_year;
